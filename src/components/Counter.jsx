f0e147b2464cd8d138042d83b1f5e527bc544926
Maria <marialopez.apc@gmail.com>
2024-12-24 11:01:12 +0100

    Add the useState hook to control the counter state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 const Counter = () => {
+    const [count, setCount] = useState(0);    
+
     return (
         <div>
             <h1>Counter</h1>
-            <h2>0</h2>
+            <h2>{count}</h2>
             <button>Incrementar</button>
             <button>Decrementar</button>
             <button>Reiniciar</button>
