bf6f7717ee2efaa5e074f53066a71b60efff93f1
Maria <marialopez.apc@gmail.com>
2024-12-24 11:06:17 +0100

    Make the buttons work

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);    
+    const [count, setCount] = useState(0);  
+    
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
 
     return (
         <div>
             <h1>Counter</h1>
             <h2>{count}</h2>
-            <button>Incrementar</button>
-            <button>Decrementar</button>
-            <button>Reiniciar</button>
+            <button onClick={increment}>Incrementar</button>
+            <button onClick={decrement}>Decrementar</button>
+            <button onClick={reset}>Reiniciar</button>
         </div>
     )
 }
